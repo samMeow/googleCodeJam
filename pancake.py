63a7b59d7ac36e04284be79cf6e4dade865176a3
samuel <kwokyinlun@gmail.com>
2017-04-07 19:34:50 +0800

    init pancake

---
+++ pancake.py
@@ -0,0 +1,48 @@
+import sys
+from itertools import repeat
+
+if len(sys.argv) < 3:
+	print("Usage `python3 base input_file output_file`")
+	sys.exit(1)
+
+def readFile(filename):
+	data = []
+	with open(filename) as f:
+		line = f.readline()
+		for datum in f:
+			data.append(datum.strip('\n'))
+		f.close()
+	return data
+
+def filp_cake(cake):
+	return "-" if cake == "+" else "+"
+
+def processDatum(datum):
+	pancake_list = list(datum)
+
+	count = 0
+	current_cake = "+";
+	reversed_list = list(reversed(pancake_list))
+
+	for pancake in reversed_list:
+		if(pancake != current_cake):
+			current_cake = filp_cake(current_cake)
+			count += 1
+
+	return count
+
+def outputFormat(case, result):
+	return 'Case #{0}: {1}\n'.format(case + 1, result)
+
+def writeFile(filename, data):
+	fout = open(filename, "w")
+	for idx, val in enumerate(data):
+		fout.write(outputFormat(idx, val))
+	fout.close()
+
+def main():
+	data = readFile(sys.argv[1]);
+	writeFile(sys.argv[2], list(map(processDatum, data)))
+
+if __name__ == "__main__":
+	main();
